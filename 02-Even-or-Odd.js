7dd43d559685642853b94d2129d66cd915907219
saif96-max <kattan.saifali@gmail.com>
2023-06-05 11:15:29 +0200

    second exercis is complet

---
+++ 02-Even-or-Odd.js
@@ -0,0 +1,13 @@
+//   exercis 2
+//   Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(chiffre) {
+  
+    if (chiffre % 2 === 0){
+      return "Even";
+      
+    } else {
+      return "Odd";
+    }
+      
+  }
