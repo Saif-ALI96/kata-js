8d1855e9924646c2c30aeb9e11762966b42bafd0
saif96-max <kattan.saifali@gmail.com>
2023-06-05 11:38:08 +0200

    the check is completed

---
+++ 03-Opposite-number.js
@@ -0,0 +1,15 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+    //your code here
+    return - number;
+  }
+
+//   another way to get the same result
+
+function opposite(number) {
+    return number * (-1);
+}
+
+// the same thing
+const opposite = number => -number;
