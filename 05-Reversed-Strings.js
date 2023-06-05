99d86c684b44f41516db879ada70a22bf4ee5266
saif96-max <kattan.saifali@gmail.com>
2023-06-05 12:08:37 +0200

    check is completed

---
+++ 05-Reversed-Strings.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str){
+  
+    return str.split(""). reverse().join("");
+    
+  }
+
+  //   another way to get the same result
+
+  const solution = str => str.split('').reverse().join(''); 
