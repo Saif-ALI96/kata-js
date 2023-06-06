440d09571a9365a109e8229ce1a48929a0952d40
saif96-max <kattan.saifali@gmail.com>
2023-06-06 12:32:58 +0200

    check is completed

---
+++ 16-You-Can't-Code-Under-Pressure.js
@@ -0,0 +1,20 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    
+    return i*2;
+  }
+   //   another way to get the same result
+
+   const doubleInteger = i => 2 * i;
+
+
+   //   another way to get the same result
+
+function doubleInteger(i) {
+
+    return i+i;
+ }
+  
