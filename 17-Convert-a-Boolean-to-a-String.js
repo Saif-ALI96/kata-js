2af6fc7ce6b5022090fafc14379db26e448e594b
saif96-max <kattan.saifali@gmail.com>
2023-06-06 12:41:15 +0200

    check is completed

---
+++ 17-Convert-a-Boolean-to-a-String.js
@@ -0,0 +1,23 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    //your code here
+    
+    
+    return b = String(b);
+  }
+
+   //   another way to get the same result
+
+   function booleanToString(b){
+    return b.toString();
+  }
+
+   //   another way to get the same result
+
+
+   function booleanToString(b){
+    return ''+ b;
+  }
+
+
