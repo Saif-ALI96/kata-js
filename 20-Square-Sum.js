25209a3e610ed8b283c9e3627d83d53c7a7be92d
saif96-max <kattan.saifali@gmail.com>
2023-06-07 16:19:54 +0200

    check is completed

---
+++ 20-Square-Sum.js
@@ -0,0 +1,34 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 becaus
+
+
+function squareSum(numbers){
+    let sum = 0;
+    
+   for(let i =0; i < numbers.length ; i++){
+     
+      sum += numbers[i]**2;
+     
+   }
+   
+ return sum;
+ }
+
+
+   //   another way to get the same result
+
+
+   function squareSum(numbers)
+{ 
+  let totalSum = 0;
+  for (i=0;i<numbers.length;i++)
+  {
+    totalSum += Math.pow(numbers[i], 2);
+  }
+  return totalSum;
+}
+
+
+
+
